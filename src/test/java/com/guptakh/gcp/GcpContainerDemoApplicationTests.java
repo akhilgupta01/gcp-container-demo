bc6124c443105435027e792f1dfef6548df17faa
Akhil Gupta <akhilgupta01@gmail.com>
2021-08-17 11:13:42 +0530

    initial draft

---
+++ src/test/java/com/guptakh/gcp/GcpContainerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guptakh.gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpContainerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
