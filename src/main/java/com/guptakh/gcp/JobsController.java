728d5ef31d01f47b3572f373b453c5fc0055512b
Akhil Gupta <akhilgupta01@gmail.com>
2021-08-17 12:09:02 +0530

    fixed bug

--- src/main/java/com/guptakh/gcp/JobsController.java
+++ src/main/java/com/guptakh/gcp/JobsController.java
@@ -13,17 +13,15 @@ public class JobsController {
     @GetMapping("/jobs/{jobName}")
     public String runJob(@PathVariable String jobName){
         System.out.println("Starting job " + jobName);
-        List<Integer> primeNumbers = new ArrayList<>();
+        final List<Integer> primeNumbers = new ArrayList<>();
         for(int i=2; i<100000; i++){
             if(i%2 == 0){
                 continue;
             }
-            for(int primeNum : primeNumbers){
-                if(i % primeNum == 0){
-                    continue;
-                }
+            final int num = i;
+            if(primeNumbers.stream().noneMatch(x -> num % x == 0)){
+                primeNumbers.add(i);
             }
-            primeNumbers.add(i);
         }
         return String.valueOf(primeNumbers);
     }
